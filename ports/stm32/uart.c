3ac5c3b28c91a91d275ecc529c9cac08afa074b3
iabdalkader <i.abdalkader@gmail.com>
2025-09-16 17:27:17 +0200

    stm32/uart: Enable UART FIFO for STM32N6.
    Enable UART FIFO for STM32N6.
    
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/stm32/uart.c
+++ ports/stm32/uart.c
@@ -686,7 +686,7 @@ bool uart_init(machine_uart_obj_t *uart_obj,
     huart.Init.HwFlowCtl = flow;
     huart.Init.OverSampling = UART_OVERSAMPLING_16;
 
-    #if defined(STM32G4) || defined(STM32H7) // WB also has a fifo..
+    #if defined(STM32G4) || defined(STM32H7) || defined(STM32N6) // WB also has a fifo..
     huart.FifoMode = UART_FIFOMODE_ENABLE;
     #endif
 
@@ -738,7 +738,7 @@ bool uart_init(machine_uart_obj_t *uart_obj,
         uart_obj->char_width = CHAR_WIDTH_8BIT;
     }
 
-    #if defined(STM32H7)
+    #if defined(STM32H7) || defined(STM32N6)
     HAL_UARTEx_SetTxFifoThreshold(&huart, UART_TXFIFO_THRESHOLD_1_8);
     HAL_UARTEx_SetRxFifoThreshold(&huart, UART_RXFIFO_THRESHOLD_1_8);
     HAL_UARTEx_EnableFifoMode(&huart);
@@ -1224,7 +1224,7 @@ size_t uart_tx_data(machine_uart_obj_t *self, const void *src_in, size_t num_cha
     // timeout_char by FIFO size + 1.
     // STM32G4 has 8 words FIFO.
     timeout = (8 + 1) * self->timeout_char;
-    #elif defined(STM32H7)
+    #elif defined(STM32H7) || defined(STM32N6)
     // STM32H7 has 16 words FIFO.
     timeout = (16 + 1) * self->timeout_char;
     #else
